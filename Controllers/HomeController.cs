8595a9eb3337fcf316c2169530551ef7b3eb8be8
skyler marquis <skylerjmarquis@gmail.com>
2024-01-17 23:51:09 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ch02FutureValueMarquis.Models;
+public class HomeController : Controller
+    {
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.FV = 0;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(FutureValueModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+        ViewBag.FV = 0;
+        return View(model);
+    }
+}
